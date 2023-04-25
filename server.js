81dcb7469aad7f561423148af1bc2def51cdd2ab
root <root@Vivo.ru>
2023-04-25 23:34:56 +0600

    CORS3

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({credentials: true, origin: 'http://localhost:5173'}))
+app.use(cors({credentials: true, origin: 'http://127.0.0.1:5173'}))
 app.use('/api', UserRouter)
 app.use(ErrorMiddleware)
 
