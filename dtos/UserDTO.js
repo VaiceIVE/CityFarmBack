c8c4bbc0f8a44a1b071b0f52eb58c28b40b61732
root <root@Vivo.ru>
2023-05-11 22:36:22 +0600

    DTO update

--- dtos/UserDTO.js
+++ dtos/UserDTO.js
@@ -3,11 +3,17 @@ module.exports = class UserDto
     email;
     id;
     isActivated;
+    name;
+    lastname;
+    nickname;
 
     constructor(model)
     {
         this.email = model.email;
         this.id = model._id;
         this.isActivated = model.isActivated;
+        this.name = model.name;
+        this.lastname = model.lastname;
+        this.nickname = model.nickname;
     }
 }
